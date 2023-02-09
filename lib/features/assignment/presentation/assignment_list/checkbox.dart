24df59cba97c8a04940aaf1b86d1f09b3c7cd8b1
annika wayne <annika.wayne@fivepackcreative.com>
2023-02-09 14:01:35 -0600

    simplify animated check

--- lib/features/assignment/presentation/assignment_list/checkbox.dart
+++ lib/features/assignment/presentation/assignment_list/checkbox.dart
@@ -22,90 +22,80 @@ class AssignmentCheckBox extends ConsumerWidget {
 }
 
 class _AnimatedCheck extends StatefulWidget {
-  const _AnimatedCheck({super.key});
+  const _AnimatedCheck();
 
   @override
   State<_AnimatedCheck> createState() => _AnimatedCheckState();
 }
 
 class _AnimatedCheckState extends State<_AnimatedCheck>
-    with TickerProviderStateMixin {
-  late AnimationController _controller;
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> shrink;
+  late Animation<double> grow;
+  late Animation<Color?> color;
 
   @override
   void initState() {
     super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 1500), vsync: this);
+    // Animations defined here
+    shrink = Tween<double>(begin: 38.0, end: 0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.0, 0.2, curve: Curves.easeInBack),
+      ),
+    )..addListener(() => setState(() {}));
 
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 1500),
-      vsync: this,
-    );
+    grow = Tween<double>(begin: 0.0, end: 38.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.2, 1.0, curve: Curves.elasticOut),
+      ),
+    )..addListener(() => setState(() {}));
+
+    color = ColorTween(
+      begin: AppColors.text,
+      end: AppColors.darkPrimary,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.2, 0.8, curve: Curves.ease),
+      ),
+    )..addListener(() => setState(() {}));
+
+    controller.forward();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    _controller.forward();
-    return _CheckAnim(controller: _controller.view);
-  }
-}
-
-class _CheckAnim extends StatelessWidget {
-  _CheckAnim({super.key, required this.controller})
-      : shrink = Tween<double>(begin: 38.0, end: 0.0).animate(
-          CurvedAnimation(
-              parent: controller,
-              curve: const Interval(0.0, 0.2, curve: Curves.easeInBack)),
-        ),
-        grow = Tween<double>(begin: 0.0, end: 38.0).animate(
-          CurvedAnimation(
-              parent: controller,
-              curve: const Interval(0.2, 1.0, curve: Curves.elasticOut)),
-        ),
-        color = ColorTween(begin: AppColors.text, end: AppColors.darkPrimary)
-            .animate(
-          CurvedAnimation(
-              parent: controller,
-              curve: const Interval(0.2, 0.8, curve: Curves.ease)),
-        );
-
-  final Animation<double> controller;
-  final Animation<double> shrink;
-  final Animation<double> grow;
-  final Animation<Color?> color;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (context, child) {
-        return SizedBox(
-          width: 38,
-          height: 38,
-          child: Stack(
-            children: [
-              Center(
-                child: Icon(
-                  Icons.circle_outlined,
-                  size: shrink.value,
-                ),
-              ),
-              Center(
-                child: Icon(
-                  Icons.check_circle,
-                  size: grow.value,
-                  color: color.value,
-                ),
-              ),
-            ],
+    return SizedBox(
+      height: 38,
+      width: 38,
+      child: Stack(
+        children: [
+          Center(
+            child: Icon(
+              Icons.circle_outlined,
+              size: shrink.value,
+            ),
           ),
-        );
-      },
+          Center(
+            child: Icon(
+              Icons.check_circle,
+              size: grow.value,
+              color: color.value,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
